a17fd128e6873fbc3e8543b081f50a6ee2b5de39
Anas Mostafa <anas20chakir@outlook.com>
2021-06-10 22:49:25 +0100

    Add RepoCollection Component and Update App style
    - Create RepoCollection Component and adopt github repos array as its state.
    - Fetch repos on ComponentDidMount.
    - Add App background animation.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
+import RepoCollection from './components/repo-collection';
 
 function App() {
   return (
     <div className="App">
       <h1 className="title"><span className="star">&#10022;</span> Most Starred Github Repos <span className="star">&#10022;</span></h1>
+      <RepoCollection />
     </div>
   );
 }
